4242e348f8fc4b0af7b2b218c1c46058971d78e3
PauChavero <a22pauchagor@inspedralbes.cat>
2023-10-24 10:35:41 +0200

    Pr
    Todo

---
+++ app/src/main/java/com/example/proj1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.proj1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonIrAMenu = findViewById(R.id.buttonInsert);
+
+        botonIrAMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuProductos.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
